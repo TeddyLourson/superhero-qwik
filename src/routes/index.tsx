e67cf6948c8edcb892c9df569ba5d4ce4310cba5
TeddyLourson <todorovteodortt@hotmail.com>
2023-10-09 21:06:52 +0200

    fixed error

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
-import { Resource, component$ } from "@builder.io/qwik";
+import { $, Resource, component$ } from "@builder.io/qwik";
 import { browserClient } from "~/data/browser";
 import { z, type DocumentHead, routeLoader$ } from "@builder.io/qwik-city";
 import { SuperheroCard } from "~/components/superhero-card/superhero-card";
@@ -6,32 +6,35 @@ import { createServerAPIClient } from "~/data/server";
 import { Superhero } from "~/types/superhero";
 
 // Server
-export const UseSuperheroes = z.object({
-  superheroes: Superhero.array(),
-});
-export type UseSuperheroes = z.infer<typeof UseSuperheroes>;
-export const useSuperheroes = routeLoader$<UseSuperheroes>(
-  async (requestEvent) => {
-    const api = createServerAPIClient(requestEvent);
-    const res = await api.superheroes.getSuperheroes({});
-    return { superheroes: res.superheroes };
-  }
-);
+// export const UseSuperheroes = z.object({
+//   superheroes: Superhero.array(),
+// });
+// export type UseSuperheroes = z.infer<typeof UseSuperheroes>;
+// export const useSuperheroes = routeLoader$<UseSuperheroes>(
+//   async (requestEvent) => {
+//     const api = createServerAPIClient(requestEvent);
+//     const res = await api.superheroes.getSuperheroes({});
+//     return { superheroes: res.superheroes };
+//   }
+// );
 
 export default component$(() => {
-  const res = useSuperheroes();
+  // const res = useSuperheroes();
+  const getSuperheroes = $(async () => {
+    return await browserClient.superheroes.getSuperheroes({});
+  });
 
   return (
     <>
       <h1>Retrieved data :</h1>
       {/* Server */}
-      {res.value.superheroes.map((superhero) => (
+      {/* {res.value.superheroes.map((superhero) => (
         <SuperheroCard key={superhero.id} superhero={superhero} />
-      ))}
+      ))} */}
 
       {/* Browser */}
       <Resource
-        value={browserClient.superheroes.getSuperheroes({})}
+        value={getSuperheroes()}
         onResolved={(res) => (
           <>
             {res.superheroes.map((superhero) => (
